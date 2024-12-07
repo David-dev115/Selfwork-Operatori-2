cc3eb2013417d42d5271721850377679a3914693
David Paparo <d.paparo@tiscali.it>
2024-12-07 11:27:04 +0100

    esercizio completo

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 
 
-let prova = `ciao a tutti`
 
-console.log(prova);
+let current_year = 2024;
+
+let year_of_birth = 1987;
+
+let age = current_year - year_of_birth;
+
+const GOAL = 100;
+
+let age_record = GOAL - age;
+
+
+console.log(`Hai ${age} anni e ti mancano ${age_record} per raggiungere ${GOAL} anni`);
